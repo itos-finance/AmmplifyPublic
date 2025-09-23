5298c902c523c3e22eac8ccc37bb4f3ddad37221
ItosTerence <115845392+ItosTerence@users.noreply.github.com>
2025-09-23 00:43:07 -0400

    keep the same style across walkers

--- src/walkers/Pool.sol
+++ src/walkers/Pool.sol
@@ -14,8 +14,8 @@ library PoolWalker {
 
     function settle(PoolInfo memory pInfo, int24 lowTick, int24 highTick, Data memory data) internal {
         uint24 low = pInfo.treeTick(lowTick);
-        uint24 high = pInfo.treeTick(highTick);
-        Route memory route = RouteImpl.make(pInfo.treeWidth, low, high - 1);
+        uint24 high = pInfo.treeTick(highTick) - 1;
+        Route memory route = RouteImpl.make(pInfo.treeWidth, low, high);
         route.walk(down, up, phase, WalkerLib.toRaw(data));
     }
 
