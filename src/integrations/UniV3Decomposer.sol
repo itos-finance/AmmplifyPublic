e02ea81cd7c2343d494b368e3e303055b3def2fc
brian <brian@itos.fi>
2025-11-06 11:31:48 -0600

    recalculate the liquidity

--- src/integrations/UniV3Decomposer.sol
+++ src/integrations/UniV3Decomposer.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import "forge-std/console2.sol";
 import { MakerFacet } from "../facets/Maker.sol";
 import { RFTPayer, RFTLib } from "../../lib/Commons/src/Util/RFT.sol";
 import { IRFTPayer } from "../../lib/Commons/src/Util/RFT.sol";
@@ -9,15 +8,19 @@ import { TransferHelper } from "../../lib/Commons/src/Util/TransferHelper.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { Auto165 } from  "Commons/ERC/Auto165.sol";
 
+import { TickMath } from "v4-core/libraries/TickMath.sol";
+import { IUniswapV3Pool } from 'v3-core/interfaces/IUniswapV3Pool.sol';
 import { IUniswapV3Factory } from "v3-core/interfaces/IUniswapV3Factory.sol";
+import { IUniswapV3PoolImmutables } from "v3-core/interfaces/pool/IUniswapV3PoolImmutables.sol";
 import { INonfungiblePositionManager } from "./univ3-periphery/interfaces/INonfungiblePositionManager.sol";
 import { IERC721Receiver } from "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
-import { TickMath } from "v3-core/libraries/TickMath.sol";
 
+import { PoolLib } from "../Pool.sol";
+import { LiquidityAmounts } from "./LiquidityAmounts.sol";
 import { msbBit } from "../tree/BitMath.sol";
-import { IUniswapV3PoolImmutables } from "v3-core/interfaces/pool/IUniswapV3PoolImmutables.sol";
 import { TreeTickLib } from "../tree/Tick.sol";
 import { RouteImpl, Route } from "../tree/Route.sol";
+
 
 // ─────────────────────────────────────────────────────────────────────────────
 /// @title UniV3Decomposer
@@ -51,6 +54,30 @@ contract UniV3Decomposer is RFTPayer, IERC721Receiver, Auto165 {
         MAKER = MakerFacet(_maker);
     }
 
+    /// @notice Calculates the liquidity for given token amounts in a tick range
+    /// @param poolAddr The address of the Uniswap V3 pool
+    /// @param token0 The address of token0
+    /// @param token1 The address of token1
+    /// @param tickLower The lower tick of the position
+    /// @param tickUpper The upper tick of the position
+    /// @return liquidity The calculated liquidity amount
+    function calculateLiquidity(
+        address poolAddr,
+        address token0,
+        address token1,
+        int24 tickLower,
+        int24 tickUpper
+    ) internal view returns (uint128 liquidity) {
+        (uint160 sqrtPriceX96,,,,,,) = IUniswapV3Pool(poolAddr).slot0();
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint160 sqrtPriceX96A = TickMath.getSqrtPriceAtTick(tickLower);
+        uint160 sqrtPriceX96B = TickMath.getSqrtPriceAtTick(tickUpper);
+        return LiquidityAmounts.getLiquidityForAmounts(sqrtPriceX96, sqrtPriceX96A, sqrtPriceX96B, balance0, balance1);
+    }
+
     /// @notice Calculates the liquidity offset based on tick range
     /// @param tickLower The lower tick of the position
     /// @param tickUpper The upper tick of the position
@@ -58,34 +85,16 @@ contract UniV3Decomposer is RFTPayer, IERC721Receiver, Auto165 {
     /// @return liquidityOffset The calculated liquidity offset
     function calculateLiquidityOffset(int24 tickLower, int24 tickUpper, int24 tickSpacing) internal pure returns (uint128 liquidityOffset) {
         uint24 rootWidth = TreeTickLib.calcRootWidth(TickMath.MIN_TICK, TickMath.MAX_TICK, tickSpacing);
-        console2.log("rootWidth", rootWidth);
         uint8 depth = msbBit(rootWidth);
-
-        if (tickLower == tickUpper) {
-            // Single node.
-            return 3 + (2 * depth) + 1; // 2 for rounding, 8 for potentially being the first deposit.
-        }
 
         uint24 treeLow = TreeTickLib.tickToTreeIndex(tickLower, rootWidth, tickSpacing);
         uint24 treeHigh = TreeTickLib.tickToTreeIndex(tickUpper, rootWidth, tickSpacing);
-        console2.log("treeLow", treeLow);
-        console2.log("treeHigh", treeHigh);
+
         Route memory route = RouteImpl.make(rootWidth, treeLow, treeHigh);
-        console2.log("after route");
-        uint256 lcaDepth = uint256(msbBit(route.lca.width()));
-        console2.log("after lcaDepth");
-        uint256 leftMsb = uint256(msbBit(route.left.width()));
-        console2.log("after leftMSB");
-        uint256 rightMsb = uint256(msbBit(route.right.width()));
-        console2.log("after rightMSb");
-        console2.log("after result");
-        console2.log("calculateLiquidityOffset - depth:", depth);
-        console2.log("calculateLiquidityOffset - lcaDepth:", lcaDepth);
-        console2.log("calculateLiquidityOffset - msbBit(route.left.width()):", leftMsb);
-        console2.log("calculateLiquidityOffset - msbBit(route.right.width()):", rightMsb);
-        uint128 result = uint128((2 * depth) + lcaDepth - leftMsb - rightMsb);
-        console2.log("calculateLiquidityOffset - result:", result);
-        return result + 320;
+        uint8 lcaDepth = msbBit(route.lca.width());
+        uint8 leftMsb = msbBit(route.left.width());
+        uint8 rightMsb = msbBit(route.right.width());
+        return depth + lcaDepth - leftMsb - rightMsb + 3;
     }
 
     /// @notice Prevents reentrancy by locking the contract during the call.
@@ -145,17 +154,20 @@ contract UniV3Decomposer is RFTPayer, IERC721Receiver, Auto165 {
             })
         );
         NFPM.burn(positionId);
-        console2.log("liquidity", liquidity);
 
         // 3. Recompose in Ammplify
         address factory = NFPM.factory();
         address poolAddr = IUniswapV3Factory(factory).getPool(token0, token1, fee);
         if (poolAddr == address(0)) revert PoolNotDeployed();
 
+        // we need to recalculate the liquidity given the range
+        {
+            liquidity = calculateLiquidity(poolAddr, token0, token1, tickLower, tickUpper);
+        }
+
         // Calculate dynamic liquidity offset based on tick range
         int24 tickSpacing = IUniswapV3PoolImmutables(poolAddr).tickSpacing();
         uint128 liquidityOffset = calculateLiquidityOffset(tickLower, tickUpper, tickSpacing);
-        console2.log("liquidityOffset", liquidityOffset);
         newAssetId = MAKER.newMaker(
             msg.sender,
             poolAddr,
